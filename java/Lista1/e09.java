b052c6fc53431a2dbe47dc3f413445b61c4582e7
THIAGO DA SILVA <61748855+t-silva@users.noreply.github.com>
2020-03-12 09:14:16 -0300

    Update e09.java
    /*O problema desse exercício é o tamanho do vetor. Usando vetores, eu posso estourar o limite e, em java
     não é possível realocar mais memória para o vetor, sendo necessário utilizar outro vetor e jogar o conteúdo do 
    primeiro fora.*/

--- java/Lista1/e09.java
+++ java/Lista1/e09.java
@@ -3,10 +3,10 @@ import java.util.ArrayList;
 class e09{
 	public static void main(String args[]){
 		Scanner ler = new Scanner(System.in);
-		ArrayList<Integer> lista = new ArrayList<Integer>();
-		int min,max,x,tot=0,n;
-		System.out.print("Maior valor: ");
-		max = ler.nextInt();
+		ArrayList<Integer> lista = new ArrayList<Integer>(); //dizemos que o objeto existe e colocamos na 
+		int min,max,x,tot=0,n;				    //classe correta, alocando memória. Podemos fazer na mesma 
+		System.out.print("Maior valor: ");		    //linha, ou em linhas separadas. 
+		max = ler.nextInt();					
 		System.out.print("Maior valor: ");
 		min = ler.nextInt();
 		if (max<min) {
@@ -24,11 +24,9 @@ class e09{
 			}
 			else if(x!=0) System.out.println("Valor fora do intervalo");
 		}while(x!=0);
-		System.out.println("Vetor = "+lista);
-		System.out.println("Total = "+tot);
+		System.out.println("Vetor = "+lista); //Uma opção é utilizar o lista.get (dentro de um for, para exibir um 
+		System.out.println("Total = "+tot);     //item por vez
 		System.out.println("Elementos contabilizados = "+lista.size());
 	}
 }
-/*O problema desse exercício é o tamanho do vetor. Usando vetores, eu posso estourar o limite e, em java
- não é possível realocar mais memória para o vetor, sendo necessário utilizar outro vetor e jogar o conteúdo do 
-primeiro fora.*/
+
